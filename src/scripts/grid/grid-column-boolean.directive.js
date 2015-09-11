8f772167bce11b65b9d1bca877964bd27e43265e
Andrey Pushkarev <fealaer@gmail.com>
2015-09-11 13:48:13 +0800

    Added dummy implementation of directive grid-column-boolean and injected it into the grid

--- src/scripts/grid/grid-column-boolean.directive.js
+++ src/scripts/grid/grid-column-boolean.directive.js
@@ -3,29 +3,28 @@
 
   angular
     .module('grid')
-    .controller('GridColumnStringCtrl', GridColumnStringCtrl)
-    .directive('gridColumnString', gridColumnString);
+    .controller('GridColumnBooleanCtrl', GridColumnBooleanCtrl)
+    .directive('gridColumnBoolean', gridColumnBoolean);
 
-  function GridColumnStringCtrl() {
+  function GridColumnBooleanCtrl() {
     var vm = this;
     vm.value = vm.entity[vm.field];
   }
 
-  function gridColumnString() {
+  function gridColumnBoolean() {
     return {
       restrict: 'A',
       require: '^grid',
       scope: {
         entity: '=',
-        field: '@',
-        editable: '='
+        field: '@'
       },
-      controller: 'GridColumnStringCtrl',
+      controller: 'GridColumnBooleanCtrl',
       controllerAs: 'vm',
       bindToController: true,
       transclude: true,
       replace: true,
-      templateUrl: 'scripts/grid/grid-column-string.html'
+      templateUrl: 'scripts/grid/grid-column-boolean.html'
     };
   }
 
